9a273cd8a856b5e84e9fac6714adcd613197985c
Akshit Agarwal <akshit011@gmail.com>
2021-02-26 22:12:53 +0530

    add

--- IdleChat/settings.py
+++ IdleChat/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,6 +23,8 @@ from pathlib import Path
 from decouple import config
 
 DEBUG = config('DEBUG', default=False, cast=bool)
+
+SECRET_KEY = config("SECRET_KEY")
 
 ALLOWED_HOSTS = ["139.59.39.113",]
 
