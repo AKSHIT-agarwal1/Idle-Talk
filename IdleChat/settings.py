a3999630ef7c7615cdc0acebf8dd12556ed1ed9e
Akshit Agarwal <akshit011@gmail.com>
2021-02-27 11:21:15 +0530

    update prod

--- IdleChat/settings.py
+++ IdleChat/settings.py
@@ -26,7 +26,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 SECRET_KEY = config("SECRET_KEY")
 
-ALLOWED_HOSTS = ["139.59.39.113",]
+ALLOWED_HOSTS = ["139.59.8.238",]
 
 ROOT_URLCONF = f'{config("PROJECT_NAME")}.urls'
 
@@ -190,4 +190,4 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'CodingWithMitch Team <noreply@codingwithmitch.com>'
 
 
-BASE_URL = "http://139.59.39.113"
+BASE_URL = "http://139.59.8.238"
